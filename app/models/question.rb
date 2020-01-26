a2eb25658122b184271530204c7285070a6b86bf
kamekona <leshik89@gmail.com>
2020-01-27 01:28:17 +0300

    Validates corrected according styleguide

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
 
   belongs_to :user
-  validates :text, :user, presence: true
-  validates_length_of :text, maximum: 255, message: "less than 255 if you don't mind"
+
+  validates :text, presence: true, length: { maximum: 255 }
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,32 +5,17 @@ class User < ApplicationRecord
   ITERATIONS = 20000
   DIGEST = OpenSSL::Digest::SHA256.new
 
-  has_many :questions
-  validates :email, :username, presence: true
-  validates :email, :username, uniqueness: true
-  validates :email, format: { with: /.+@.+\..+/ }
-  validates_length_of :username, maximum: 40, message: "less than 40 if you don't mind"
-  validates :username, format: { with: /\A\w+\z/ }
-
   attr_accessor :password
 
-  validates_presence_of :password, on: :create
-  validates_confirmation_of :password
+  has_many :questions
 
   before_save :encrypt_password
 
-  def encrypt_password
-    if self.password.present?
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-      self.password_hash = User.hash_to_string(
-        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
-      )
-    end
-  end
-
-  def self.hash_to_string(password_hash)
-    password_hash.unpack('H*')[0]
-  end
+  validates :email, :username, presence: true, uniqueness: true
+  validates :email, format: { with: /.+@.+\..+/ }
+  validates :username, length: { maximum: 40 }, format: { with: /\A\w+\z/ }
+  validates_presence_of :password, on: :create
+  validates_confirmation_of :password
 
   def self.authenticate(email, password)
     user = find_by(email: email)
@@ -39,6 +24,19 @@ class User < ApplicationRecord
       user
     else
       nil
+    end
+  end
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def encrypt_password
+    if self.password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+      )
     end
   end
 end

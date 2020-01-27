e1baf9e6c773835c7c930013db318e849fd4c38c
zal-lex <leshik89@gmail.com>
2020-01-27 17:01:22 +0300

    Controller users regenerated, views added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @hello = "Привет, Лёха!"
+  	@user = User.new(
+  	  name: 'Lex',
+  	  username: 'zal-lex',
+  	  avatar_url: 'https://secure.gravatar.com/avatar/0cf1881e88da48980e84bb90161b878f'
+  		)
   end
 end

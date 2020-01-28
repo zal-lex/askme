f64122ab7f6d15dc24a07022e3d56635341b8e5a
zal-lex <leshik89@gmail.com>
2020-01-28 14:31:40 +0300

    Deleted extra migration

--- db/migrate/20200126232043_add_username_id_email_id_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddUsernameIdEmailIdToUser < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :username_id, :integer, unique: true
-    add_column :users, :email_id, :integer, unique: true
-  end
-end

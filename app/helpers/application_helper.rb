9492d0e4cad07baa248d8272c78043c71bda5486
zal-lex <leshik89@gmail.com>
2020-01-29 14:07:16 +0300

    Added new helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+# В этом файле мы можем писать вспомогательные методы (хелперы) для
+# шаблонов (view) нашего приложения.
 module ApplicationHelper
+  # Этот метод возвращает ссылку на автарку пользователя, если она у него есть.
+  # Или ссылку на дефолтную аватарку, которая лежит в app/assets/images
+  def user_avatar(user)
+    if user.avatar_url.present?
+      user.avatar_url
+    else
+      asset_path 'avatar.jpg'
+    end
+  end
 end

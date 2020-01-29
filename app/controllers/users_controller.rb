0e43fe52dea3e99408fef040a7b8f8c2a70e2933
zal-lex <leshik89@gmail.com>
2020-01-29 14:08:41 +0300

    Modified users index method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,27 @@
+# (c) goodprogrammer.ru
+#
+# Контроллер, управляющий пользователями. Должен уметь:
+#
+#   1. Показывать страницу пользователя
+#   2. Создавать новых пользователей
+#   3. Позволять пользователю редактировать свою страницу
+#
 class UsersController < ApplicationController
+  # Это действие отзывается, когда пользователь заходит по адресу /users
   def index
+    # Мы создаем массив из двух болванок пользователей. Для создания фейковой
+    # модели мы просто вызываем метод User.new, который создает модель, не
+    # записывая её в базу.
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Vadim',
+        username: 'installero',
+        avatar_url: 'https://secure.gravatar.com/avatar/' \
+          '71269686e0f757ddb4f73614f43ae445?s=100'
+      ),
+      User.new(id: 2, name: 'Misha', username: 'aristofun')
+    ]
   end
 
   def new
@@ -8,11 +30,28 @@ class UsersController < ApplicationController
   def edit
   end
 
+  # Это действие отзывается, когда пользователь заходит по адресу /users/:id,
+  # например /users/1.
   def show
-  	@user = User.new(
-  	  name: 'Lex',
-  	  username: 'zal-lex',
-  	  avatar_url: 'https://secure.gravatar.com/avatar/0cf1881e88da48980e84bb90161b878f'
-  		)
+    # Болванка пользователя
+    @user = User.new(
+      name: 'Vadim',
+      username: 'installero',
+      avatar_url: 'https://secure.gravatar.com/avatar/' \
+        '71269686e0f757ddb4f73614f43ae445?s=100'
+    )
+
+    # Болванка вопросов для пользователя
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+      Question.new(
+        text: 'В чем смысл жизни?', created_at: Date.parse('27.03.2016')
+      )
+    ]
+
+    # Болванка для нового вопроса
+    @new_question = Question.new
+
+    # Обратите внимание, пока ни одна из болванок не достается из базы
   end
 end

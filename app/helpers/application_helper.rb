a966be79714b140c11e7855607ea869996b9b2dc
zal-lex <leshik89@gmail.com>
2020-01-29 16:59:27 +0300

    Added inclination helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,31 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+	# Этот метод будет склонять слова. Для примера используем слово "енот"
+	def inclination(number, enot, enota, enotov)
+	  # Если ничего не ввели или ввели не число, считаем, что ввели 0
+	  if (number == nil || !number.is_a?(Numeric))
+	    number = 0
+	  end
+
+	  # Обрабатываем особенный интервал, где также множественное число
+	  if (11..14).include?(number % 100)
+	    return enotov
+	  end
+
+	  # Склоняем в зависимости от остатка деления на 10
+	  ostatok = number % 10
+
+	  if ostatok == 1
+	    return enot
+	  end
+
+	  if (2..4).include?(ostatok)
+	    return enota
+	  end
+
+	  if ostatok > 4 || ostatok == 0
+	    return enotov
+	  end
+	end
 end

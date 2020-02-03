f76c7d6e97fd3d63c9d0d46aa61a893841f014f6
zal-lex <leshik89@gmail.com>
2020-02-03 16:54:35 +0300

    Registration and login forms added. Sessions added.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  	@user = User.authenticate(params[:email], params[:password])
+
+  	if @user.present?
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: 'вы успешно залогинились'
+    else
+      flash.now.alert = 'Неправильный емэйл или пароль'
+      render :new
+    end
+  end
+
+  def destroy
+  end
+end

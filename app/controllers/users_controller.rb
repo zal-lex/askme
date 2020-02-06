43e20524686ad79e8c8d848f8084ee7987ced7b1
zal-lex <leshik89@gmail.com>
2020-02-06 22:03:58 +0300

    Color_code added to user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,6 +66,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation,
-                                 :name, :username, :avatar_url)
+                                 :name, :username, :avatar_url, :color_code)
   end
 end

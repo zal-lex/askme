baf1980b9799131cf47ca32600cac6faea485377
zal-lex <leshik89@gmail.com>
2020-02-08 17:55:42 +0300

    config.i18n.locale changed to :en

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module Askme
     config.time_zone = 'Moscow'
 
     config.i18n.default_locale = :en
-    config.i18n.locale = :ru
+    config.i18n.locale = :en
 
     config.i18n.fallbacks = [:en]
   end

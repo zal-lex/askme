60261583016555ffbf9417c5aa6b7b9958c57c89
zal-lex <leshik89@gmail.com>
2020-02-11 00:04:48 +0300

    Fixed using custom scope

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :authorize_user, except: [:index, :new, :create, :show]
 
   def index
-    @users = User.all.sorted
+    @users = User.sorted
   end
 
   def new

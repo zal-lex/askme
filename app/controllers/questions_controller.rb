c22120d9fb543cb7c99548a20019949ea41d284b
zal-lex <leshik89@gmail.com>
2020-02-12 00:01:06 +0300

    Added author to question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,6 +11,8 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
 
+    @question.author = current_user
+
     if @question.save
       redirect_to user_path(@question.user), notice: 'Вопрос задан'
     else

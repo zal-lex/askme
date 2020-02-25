83b6e18cbb062559e7192ba4aa524be3463edfd8
zal-lex <leshik89@gmail.com>
2020-02-26 01:15:32 +0300

    Set_hashtag method added

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,7 +1,6 @@
 class Hashtag < ApplicationRecord
-  REGEX_HASHTAG = /\A#[a-zа-я0-9_]+\z/
 
   has_many :hashtag_questions
   has_many :questions, through: :hashtag_questions
-  validates :name, format: { with: REGEX_HASHTAG }, presence: true
+  validates :name, presence: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,24 @@
 class Question < ApplicationRecord
+  REGEX_HASHTAG = /\A#[a-zа-я0-9_]+\z/
 
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
   has_many :hashtag_questions
   has_many :hashtags, through: :hashtag_questions
-
+  before_save :set_hashtags
   validates :text, presence: true, length: { maximum: 255 }
 
+  def set_hashtags
+    text.scan(REGEX_HASHTAG).each do |substr|
+      next if substr.length > 25
+      hashtag = Hashtag.find_by(name: substr)
+      if hashtag
+        hashtag.touch
+      else
+        hashtag = Hashtag.new(name: substr)
+        hashtag.save!
+      end
+      hashtags << hashtag
+    end
+  end
 end

026b5c152bdd791a1fb142d56518a2db133a328b
zal-lex <leshik89@gmail.com>
2020-03-04 21:17:21 +0300

    Delete duplicated tagging

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,17 +4,21 @@ class Question < ApplicationRecord
   belongs_to :author, class_name: 'User', optional: true
   has_many :hashtag_questions, dependent: :destroy
   has_many :hashtags, through: :hashtag_questions
-  after_update :reset_hashtags
-  after_commit :set_hashtags
+  before_save :reset_hashtags
+  after_save :set_hashtags
   validates :text, presence: true, length: { maximum: 255 }
 
   def reset_hashtags
-    @question.hashtag_questions.delete
+    hashtag_questions.delete_all
   end
 
   def set_hashtags
+    uniq_tags = []
     "#{text} #{answer}".downcase.scan(Hashtag::REGEX_HASHTAG).each do |substr|
-      hashtags << Hashtag.find_or_create_by(name: substr)
+      unless uniq_tags.include?(substr)
+        uniq_tags << substr
+        hashtags << Hashtag.find_or_create_by(name: substr)
+      end
     end
   end
 end

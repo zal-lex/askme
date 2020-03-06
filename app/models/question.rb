6958ba467bb602160043930357e319a736556f53
zal-lex <leshik89@gmail.com>
2020-03-06 10:20:03 +0300

    Code refactoring

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,21 +4,12 @@ class Question < ApplicationRecord
   belongs_to :author, class_name: 'User', optional: true
   has_many :hashtag_questions, dependent: :destroy
   has_many :hashtags, through: :hashtag_questions
-  before_save :reset_hashtags
   after_commit :set_hashtags, on: %i[create update]
   validates :text, presence: true, length: { maximum: 255 }
 
-  def reset_hashtags
-    hashtag_questions.delete_all
-  end
-
   def set_hashtags
-    uniq_tags = []
-    "#{text} #{answer}".downcase.scan(Hashtag::REGEX_HASHTAG).each do |substr|
-      unless uniq_tags.include?(substr)
-        uniq_tags << substr
-        hashtags << Hashtag.find_or_create_by(name: substr)
-      end
-    end
+    hashtags.clear
+    "#{text} #{answer}".downcase.scan(Hashtag::REGEX_HASHTAG).uniq.each do |tag|
+      hashtags << Hashtag.find_or_create_by(name: tag)
   end
 end

9d9a3ac5b4d5cd416ee2a28d2a7bdb060f4fc42f
zal-lex <leshik89@gmail.com>
2020-03-06 21:24:15 +0300

    Resource session fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	root 'users#index'
   resources :users
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
   resources :questions, except: [:show, :new, :index]
   resources :hashtags, only: [:show]
 
